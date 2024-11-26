b3bcfc008e0d40b4915c854dc08ae0af896906de
Rachel Gagnon <rgagnon@google.com>
2024-11-26 14:44:48 +0000

    updating base images to match beam versions

--- main.py
+++ main.py
@@ -12,95 +12,30 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import os
-import logging
-import re
-import argparse
-import oracledb
 
 import apache_beam as beam
-from apache_beam.io import WriteToText
-from apache_beam.io import ReadFromText
+from apache_beam.io.textio import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.options.pipeline_options import SetupOptions
-
-logger = logging.getLogger(__name__)
 
 
-class WordExtractingDoFn(beam.DoFn):
-    """Parse each line of input text into words."""
+def write_to_cloud_storage(argv=None):
+    # Parse the pipeline options passed into the application.
+    class MyOptions(PipelineOptions):
+        @classmethod
+        # Define a custom pipeline option that specfies the Cloud Storage bucket.
+        def _add_argparse_args(cls, parser):
+            parser.add_argument("--output", required=True)
 
-    def process(self, element):
-        """Returns an iterator over the words of this element.
+    wordsList = ["1", "2", "3", "4"]
+    options = MyOptions()
 
-        The element is a line of text.  If the line is blank, note that, too.
-
-        Args:
-          element: the element being processed
-
-        Returns:
-          The processed element.
-        """
-        return re.findall(r'[\w\']+', element, re.UNICODE)
-
-
-def run(argv=None, save_main_session=True):
-    """Main entry point; defines and runs the wordcount pipeline."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--input',
-        dest='input',
-        default='gs://dataflow-samples/shakespeare/kinglear.txt',
-        help='Input file to process.')
-    parser.add_argument(
-        '--output',
-        dest='output',
-        required=True,
-        help='Output file to write results to.')
-    known_args, pipeline_args = parser.parse_known_args(argv)
-
-    # We use the save_main_session option because one or more DoFn's in this
-    # workflow rely on global context (e.g., a module imported at module level).
-    pipeline_options = PipelineOptions(pipeline_args)
-    pipeline_options.view_as(
-        SetupOptions).save_main_session = save_main_session
-
-    class LogResults(beam.DoFn):
-        """Just log the results"""
-
-        def process(self, element):
-            logging.info("LD_LIBRARY_PATH contents: %s", element)
-            yield element
-
-    # The pipeline will be run on exiting the with block.
-    with beam.Pipeline(options=pipeline_options) as p:
-        oracledb.init_oracle_client()
-
+    with beam.Pipeline(options=options) as pipeline:
         (
-            p | 'Create' >> beam.Create(
-                [os.listdir('/opt/oracle/instantclient_23_5')])
-              | beam.ParDo(LogResults())
+            pipeline
+            | "Create elements" >> beam.Create(wordsList)
+            | "Write Files" >> WriteToText(options.output, file_name_suffix=".txt")
         )
 
-        # Read the text file[pattern] into a PCollection.
-        lines = p | 'Read' >> ReadFromText(known_args.input)
 
-        counts = (
-            lines
-            | 'Split' >> (beam.ParDo(WordExtractingDoFn()).with_output_types(str))
-            | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
-            | 'GroupAndSum' >> beam.CombinePerKey(sum))
-
-        # Format the counts into a PCollection of strings.
-        def format_result(word, count):
-            return '%s: %d' % (word, count)
-
-        output = counts | 'Format' >> beam.MapTuple(format_result)
-
-        # Write the output using a "Write" transform that has side effects.
-        output | 'Write' >> WriteToText(known_args.output)
-
-
-if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.INFO)
-    run()
+if __name__ == "__main__":
+    write_to_cloud_storage()
